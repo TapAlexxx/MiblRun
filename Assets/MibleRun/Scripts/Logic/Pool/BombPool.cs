66c13907a78f9c570d55913594c276ecf6f83ebf
TapAlexxx <lordpictures123@gmail.com>
2023-05-17 09:51:26 +0300

    implement basic pools

---
+++ Assets/MibleRun/Scripts/Logic/Pool/BombPool.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Scripts.Logic.BombControl;
+using UnityEngine;
+
+namespace Scripts.Logic.Pool
+{
+
+    public class BombPool : MonoBehaviour
+    {
+        [SerializeField] private Bomb prefab;
+        [SerializeField] private int poolSize;
+
+        private List<Bomb> _pool;
+
+        public void Initialize()
+        {
+            _pool = new List<Bomb>();
+            for (int i = 0; i < poolSize; i++)
+            {
+                Bomb bomb = Instantiate(prefab, transform).GetComponent<Bomb>();
+                _pool.Add(bomb);
+                bomb.gameObject.SetActive(false);
+            }    
+        }
+        
+        public bool TryGetBomb(out Bomb bomb)
+        {
+            bomb = _pool.FirstOrDefault(x => x.gameObject.activeSelf == false);
+            return bomb != null;
+        }
+    }
+
+}
---
+++ Assets/MibleRun/Scripts/Logic/Pool/EnemyPool.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Scripts.Logic.EnemyControl;
+using UnityEngine;
+
+namespace Scripts.Logic.Pool
+{
+
+    public class EnemyPool : MonoBehaviour
+    {
+        [SerializeField] private Enemy prefab;
+        [SerializeField] private int poolSize;
+
+        private List<Enemy> _pool;
+
+        public void Initialize()
+        {
+            _pool = new List<Enemy>();
+            for (int i = 0; i < poolSize; i++)
+            {
+                Enemy bomb = Instantiate(prefab, transform).GetComponent<Enemy>();
+                _pool.Add(bomb);
+                bomb.gameObject.SetActive(false);
+            }    
+        }
+        
+        public bool TryGetEnemy(out Enemy enemy)
+        {
+            enemy = _pool.FirstOrDefault(x => x.gameObject.activeSelf == false);
+            return enemy != null;
+        }
+    }
+
+}
