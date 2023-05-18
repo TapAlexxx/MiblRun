025139d4bd44e6e423223fd1848fc03896d99391
TapAlexxx <lordpictures123@gmail.com>
2023-05-18 18:49:50 +0300

    usingJob system for tests

--- Assets/MibleRun/Scripts/Logic/Unit/UnitMovement.cs
+++ Assets/MibleRun/Scripts/Logic/Unit/UnitMovement.cs
@@ -1,5 +1,9 @@
-﻿using Scripts.Extensions;
+﻿using System;
+using Scripts.Extensions;
 using Scripts.StaticClasses;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
 using UnityEngine;
 
 namespace Scripts.Logic.Unit
@@ -16,6 +20,8 @@ namespace Scripts.Logic.Unit
         private Vector3 _targetVelocity;
         private float _speed;
         private bool _active;
+        private JobHandle _directionJobHandle;
+        private NativeArray<Vector3> _directionResult;
 
         public float Speed => _speed;
         public Vector3 Direction => _direction;
@@ -47,7 +53,98 @@ namespace Scripts.Logic.Unit
 
         private void FixedUpdate()
         {
-            Move();
+            if(!_active)
+                return;
+            
+            _targetVelocity = Vector3.zero;
+            if (_direction.sqrMagnitude < Constants.Epsilon) 
+                return;
+            
+            _directionResult = new NativeArray<Vector3>(2, Allocator.TempJob);
+            DirectionJob job = new DirectionJob
+            {
+                Direction = _direction,
+                Speed = _speed,
+                SmoothedVelocity = _smoothedVelocity,
+                TargetVelocity = _targetVelocity, 
+                CurrentVelocity = _currentVelocity,
+                DeltaTime = Time.deltaTime,
+                RigidBodyPos = unitRigidbody.position,
+                CurrentPosition = transform.position,
+                Result = _directionResult
+            };
+            _directionJobHandle = job.Schedule();
+            _directionJobHandle.Complete();
+
+            _smoothedVelocity = _directionResult[1];
+            unitRigidbody.MovePosition(_directionResult[0]);
+
+            _directionResult.Dispose();
+        }
+
+        /*
+        private void FixedUpdate()
+        {
+            if(!_active)
+                return;
+            //Move();
+
+            _targetVelocity = Vector3.zero;
+            if (_direction.sqrMagnitude < Constants.Epsilon) 
+                return;
+            
+            _directionResult = new NativeArray<Vector3>(3, Allocator.TempJob);
+            DirectionJob job = new DirectionJob
+            {
+                Direction = _direction,
+                Speed = _speed,
+                SmoothedVelocity = _smoothedVelocity,
+                TargetVelocity = _targetVelocity, 
+                CurrentVelocity = _currentVelocity,
+                DeltaTime = Time.deltaTime,
+                RigidBodyPos = unitRigidbody.position,
+                CurrentPosition = transform.position,
+                Result = _directionResult
+            };
+            _directionJobHandle = job.Schedule();
+            _directionJobHandle.Complete();
+
+            Debug.Log($"{unitRigidbody.position} -> {_directionResult[0]}");
+            unitRigidbody.MovePosition(_directionResult[0]);
+            _smoothedVelocity = _directionResult[1];
+            _targetVelocity = _directionResult[2];
+            
+            _directionResult.Dispose();
+        }
+        */
+
+        [BurstCompile]
+        private struct DirectionJob : IJob
+        {
+            public Vector3 Direction;
+            public float   Speed;
+            public Vector3 SmoothedVelocity;
+            public Vector3 TargetVelocity;
+            public Vector3 CurrentVelocity;
+            public float   DeltaTime;
+            public Vector3 RigidBodyPos;
+            public Vector3 CurrentPosition;
+
+            public NativeArray<Vector3> Result;
+
+            public void Execute()
+            {
+                TargetVelocity = Direction * Speed;
+                
+                SmoothedVelocity = Vector3
+                    .SmoothDamp(SmoothedVelocity, TargetVelocity, ref CurrentVelocity, 0.1f,
+                        float.PositiveInfinity, DeltaTime);
+                Vector3 targetPosition = RigidBodyPos + SmoothedVelocity * DeltaTime;
+                targetPosition.SetY(CurrentPosition.y);
+                
+                Result[0] = targetPosition;
+                Result[1] = SmoothedVelocity;
+            }
         }
 
         private void Move()
