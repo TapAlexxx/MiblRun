2f5a7c3bc71da2698d9d13869f70873ce94941bc
TapAlexxx <lordpictures123@gmail.com>
2023-05-18 16:40:11 +0300

    enemy movement by job system

--- Assets/MibleRun/Scripts/Logic/EnemyControl/EnemyMover.cs
+++ Assets/MibleRun/Scripts/Logic/EnemyControl/EnemyMover.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Scripts.Logic.PlayerControl;
 using Scripts.Logic.Unit;
+using Unity.Collections;
+using Unity.Jobs;
 using UnityEngine;
 
 namespace Scripts.Logic.EnemyControl
@@ -12,7 +14,7 @@ namespace Scripts.Logic.EnemyControl
         
         private Transform _player;
         private PlayerHealth _playerHealth;
-
+        
         private void OnValidate()
         {
             if (!unitMovement) TryGetComponent(out unitMovement);
@@ -24,6 +26,19 @@ namespace Scripts.Logic.EnemyControl
             _playerHealth = _player.GetComponent<PlayerHealth>();
         }
 
+        private struct DirectionJob : IJob
+        {
+            public Vector3 PlayerPosition;
+            public Vector3 EnemyPosition;
+            public NativeArray<Vector3> Result;
+            
+            public void Execute()
+            {
+                Vector3 targetDirection = PlayerPosition - EnemyPosition;
+                Result[0] = targetDirection.normalized;
+            }
+        }
+
         private void Update()
         {
             if(_player == null)
@@ -31,14 +46,35 @@ namespace Scripts.Logic.EnemyControl
 
             if (!_playerHealth.IsAlive)
             {
-                unitMovement.SetMovementDirection(Vector3.zero);
-                _player = null;
+                StopMove();
                 return;
             }
+
+            NativeArray<Vector3> directionResult = new NativeArray<Vector3>(1, Allocator.TempJob);
+            DirectionJob job = new DirectionJob
+            {
+                PlayerPosition = _player.position,
+                EnemyPosition =  transform.position,
+                Result = directionResult
+            };
+            JobHandle directionJobHandle = job.Schedule();
             
+            directionJobHandle.Complete();
+            unitMovement.SetMovementDirection(directionResult[0]);
+            directionResult.Dispose();
+        }
+
+        private Vector3 CalculateTargetDirection()
+        {
             Vector3 targetDirection = _player.position - transform.position;
             targetDirection = targetDirection.normalized;
-            unitMovement.SetMovementDirection(targetDirection);
+            return targetDirection;
+        }
+
+        private void StopMove()
+        {
+            unitMovement.SetMovementDirection(Vector3.zero);
+            _player = null;
         }
 
         public void StartMove()
